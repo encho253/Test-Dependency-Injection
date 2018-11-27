825d7e05406451101e30314dbd7e083af434e146
encho253 <encho.enevski@gmail.com>
2018-11-27 15:50:56 +0200

    add project structure

---
+++ MessageSystem/MessageSystem.Startup/Startup.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MessageSystem.Startup
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
